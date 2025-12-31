fdd8657bd480be15e7a4e70ef5dbaa12a3260f66
Nandini <nandiniagarwal0804@gmail.com>
2025-12-31 17:49:08 +0530

    add CORS to allow requests from React frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 import sequelize from './config/database.js';
 import Article from './models/Article.js';
+import cors from 'cors';
 import apiRoutes from "./routes/api.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.get("/", (req, res) => {
